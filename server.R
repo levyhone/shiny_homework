de9d3c5bea7d09a59a10204da7f45de439f7f05a
levyhone <levyhone@uw.edu>
2015-06-28 05:13:16 -0500

    finish

--- server.R
+++ server.R
@@ -1,6 +1,147 @@
 library(shiny)
+library(tools)
+library(xlsx)
+library(xts)
 
 shinyServer(function(input, output){
+
+  output$note<-renderText({
+    infile<-input$file
+    if(is.null(infile)){
+       return(NULL)
+    }
+    ext<-file_ext(infile$name)
+    if(input$dataformat==1){
+      date.index="%m/%d/%Y"
+    }else if(input$dataformat==2){
+      date.index="%m-%d-%Y"
+    }
+    
+    if(input$dataformat!=3){
+    
+    if(input$filetype=="csv"){
+      if(ext!="csv"){
+        print("Please re-upload a csv file.")
+      }else{
+        data<-na.omit(read.csv(infile$datapath,header=TRUE,sep=","))
+        if(input$dataformat==1 && !(is.na(as.Date(data[,1],date.index)))){
+          print("Please go to next tab to see the interactive graph.")
+        }else if(input$dataformat==2 && !(is.na(as.Date(data[,1],date.index)))){
+          print("Please go to next tab to see the interactive graph.")
+        }else{
+          print("Data format is incorrect, please re-upload your file.")
+        }
+      }
+    }else if(input$filetype=="txt"){
+      if(ext!="txt"){
+        print("Please re-upload a txt file.")
+      }else{
+        data<-na.omit(read.table(infile$datapath,header=TRUE,sep="\t"))
+        if(input$dataformat==1 && !(is.na(as.Date(data[,1],date.index)))){
+          print("Please go to next tab to see the interactive graph.")
+        }else if(input$dataformat==2 && !(is.na(as.Date(data[,1],date.index)))){
+          print("Please go to next tab to see the interactive graph.")
+        }else{
+          print("Data format is incorrect, please re-upload your file.")
+        }
+      }
+    }
+    }else if(input$dataformat==3){
+      if(ext=="xlsx" && input$filetype=="xlsx"){
+        print("Please go to next tab to see the interactive graph.")
+      }else{
+        print("Please re-upload file.")
+      }
+      
+    }
+  })
   
+  output$graph<-renderUI({
+    plotOutput("plot", height=300,
+               click = "plot_click",
+               dblclick = dblclickOpts(
+                 id = "plot_dblclick",
+                 delay = input$dblclick_delay
+               ),
+               brush = brushOpts(
+                 id = "plot_brush",
+                 delay = input$brush_delay,
+                 delayType = input$brush_policy,
+                 direction = input$brush_dir,
+                 resetOnNew = input$brush_reset
+               )
+    )
+  })
   
+  output$plot <- renderPlot({
+    infile<-input$file
+    if(is.null(infile)){
+      return(NULL)
+    }
+    ext<-file_ext(infile$name)
+    if(input$dataformat==1){
+      date.index="%m/%d/%Y"
+    }else if(input$dataformat==2){
+      date.index="%m-%d-%Y"
+    }
+    
+    if(input$dataformat!=3){
+    if(input$filetype=="csv"){
+      if(ext!="csv"){
+        plot.new()
+      }else{
+        data<-na.omit(read.csv(infile$datapath,header=TRUE,sep=","))
+        if(input$dataformat==1 && !(is.na(as.Date(data[,1],date.index)))){
+          data<-xts(data[,2],order.by=as.Date(data[,1],date.index))
+          plot(data,type="p",pch=19,cex=1.5,main="Time Series Data")
+        }else if(input$dataformat==2 && !(is.na(as.Date(data[,1],date.index)))){
+          data<-xts(data[,2],order.by=as.Date(data[,1],date.index))
+          plot(data,type="p",pch=19,cex=1.5,main="Time Series Data")
+        }else{
+          plot.new()
+        }
+      }
+    }else if(input$filetype=="txt"){
+      if(ext!="txt"){
+        plot.new()
+      }else{
+        data<-na.omit(read.table(infile$datapath,header=TRUE,sep="\t"))
+        if(input$dataformat==1 && !(is.na(as.Date(data[,1],date.index)))){
+          data<-xts(data[,2],order.by=as.Date(data[,1],date.index))
+          plot(data,type="p",pch=19,cex=1.5,main="Time Series Data")
+        }else if(input$dataformat==2 && !(is.na(as.Date(data[,1],date.index)))){
+          data<-xts(data[,2],order.by=as.Date(data[,1],date.index))
+          plot(data,type="p",pch=19,cex=1.5,main="Time Series Data")
+        }else{
+          plot.new()
+        }
+      }
+    }
+    }else if(input$dataformat==3){
+      if(ext=="xlsx" && input$filetype=="xlsx"){
+        data<-na.omit(read.xlsx(infile$datapath,sheetName="sheet1"))
+        data<-xts(data[,2],order.by=as.Date(data[,1]))
+        plot(data,type="p",pch=19,cex=1.5,main="Time Series Data")            
+      }else{
+        plot.new()
+      }
+      
+      
+    }
+  })
+  
+  output$plot_clickinfo <- renderPrint({
+    cat("input$plot_click:\n")
+    str(input$plot_click)
+  })
+  
+  output$plot_dblclickinfo <- renderPrint({
+    cat("input$plot_dblclick:\n")
+    str(input$plot_dblclick)
+  })
+  
+  output$plot_brushinfo <- renderPrint({
+    cat("input$plot_brush:\n")
+    str(input$plot_brush)
+  })
 })
--- ui.R
+++ ui.R
@@ -1,59 +1,108 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Time Series Data Visualization"),
-  
+  titlePanel("Time Series Data Visualization"), 
   sidebarLayout(
-    sidebarPanel(img(src = "cfrm-logo.png", height = 70, width = 370),
-                 
+    sidebarPanel(img(src = "cfrm-logo.png", height = 70, width = 370), 
                  br(), br(),
-                 
+                 strong("Data Uploading Options:"),
+                 br(),br(),
                  selectInput("filetype",label="File Type:",
-                             choices=c("csv","txt","xls","xlsx"),
+                             choices=c("csv","txt","xlsx"),
                              selected="csv"),
-                 
-                 selectInput("dataformat",label="Data Format:",
-                             choices=c("Single Column Time Series Data","Multiple Column Time Series Data"),
-                             selected="Single Column Time Series Data"),
-                 
+                 selectInput("dataformat",label="Date Index Format(Data Format):",
+                             choices=c("mon/day/year"=1,"mon-day-year"=2,"number of days"=3),
+                             selected=1),
                  fileInput("file",label="Input a File:",multiple=FALSE,
                            accept = c(
-                             'text/csv',
-                             'text/comma-separated-values',
-                             'text/tab-separated-values',
-                             'text/plain',
-                             '.csv',
-                             '.xls',
-                             '.xlsx'
+                             ".csv",".txt",".xlsx"
                            )),
-                 
                  tags$hr(),
-                 
-                 checkboxInput('header', 'Header', TRUE),
-                 
-                 radioButtons('sep', 'Separator',
-                              c(Comma=',',
-                                Semicolon=';',
-                                Tab='\t'),
-                              ','),
-                 
-                 radioButtons('quote', 'Quote',
-                              c(None='',
-                                'Double Quote'='"',
-                                'Single Quote'="'"),
-                              '"')
+                 strong("Interactive Graph Options:"),
+                 br(),br(),
+                 div(class = "option-group",
+                     div(class = "option-header", strong("Double-click")),
+                     sliderInput("dblclick_delay", "Delay", min=100, max=1000, value=400,
+                                 step=100)),
+                 br(),
+                 div(class = "option-group",
+                     div(class = "option-header", strong("Brush")),
+                     radioButtons("brush_dir", "Direction(s)",
+                                  c("xy", "x", "y"), inline = TRUE),
+                     radioButtons("brush_policy", "Input rate policy",
+                                  c("debounce", "throttle"), inline = TRUE),
+                     sliderInput("brush_delay", "Delay", min=100, max=1000, value=200,
+                                 step=100),
+                     checkboxInput("brush_reset", "Reset on new image"))
+
     ),
     mainPanel(
       tabsetPanel(
-        tabPanel("Data Instruction", textOutput("data")), 
-        tabPanel("Interactive Base Graphic", plotOutput("graph"))
+        tabPanel(title="Data Instruction", value="data",
+                 fluidRow(
+                   column(9,
+                       h5(tags$u('Application Instruction')),
+                       p("This application is aimed for time series data visualization using interactive base graph. Users
+                          need to upload their time series data. And this application can be accessed on GitHub with the ",
+                       a("repository.",href='https://github.com/levyhone/shiny_homework'),target="_blank"),
+                                 
+                       h5(tags$u('Uploaded Data Requirements')),
+                       p("There are some file type and data format constraints for the uploaded data. Specifically speaking, the
+                          file types should be: .csv, .txt, .xls, .xlsx"),
+                       p("And the time series data should include a column of date index. The data format should be ",
+                         strong("(example conditional on data format choice):")),
+                       tags$ul(
+                         tags$li(p("mon/day/year"),
+                                 conditionalPanel(condition='input.dataformat==1',
+                                 fluidRow(
+                                 column(8,
+                                 tags$pre(id='ex1',
+                                          'DATE           DJIA
+12/02/1991	33.43
+12/03/1991	32.28'
+                                   ))))),
+                         tags$li(p("mon-day-year"),
+                                 conditionalPanel(condition='input.dataformat==2',
+                                 fluidRow(
+                                   column(11,
+                                          tags$pre(id="ex2",
+                                                   'DATE           DJIA
+12-02-1991      33.43
+12-03-1991	32.28'))))),
+                         tags$li(p("number of days"),
+                                 conditionalPanel(condition='input.dataformat==3',
+                                                  fluidRow(
+                                                    column(11,
+                                                           tags$pre(id="ex2",
+                                                                    'DATE         DJIA
+33574      33.43
+33575      32.28')))))
+                      ),
+                      h5(tags$u("Uploaded Data Notification:")),
+                      p("If the user uploaded data with a wrong file type or data format, then shiny would remind the user below with: "),
+                      em("Please re-upload your file."),
+                      br(),br(),
+                      p("If the file uploaded is in a right file type and data format, then shiny would display the message below:"),
+                      em("Please go to next tab to see the interactive graph."),
+                      br(),br(),
+                      strong(em(textOutput("note")))
+            
+                   )        
+                )
+        ),
+        tabPanel("Interactive Base Graphic", 
+                 fluidRow(
+                   column(10,offset=1,uiOutput("graph"))),
+                 fluidRow(
+                   column(4,verbatimTextOutput("plot_clickinfo")),
+                   column(4,verbatimTextOutput("plot_dblclickinfo")),
+                   column(4,verbatimTextOutput("plot_brushinfo"))
+                 )
       )
     )
   )
-))
+ )
+)
+)
 
 
-?datasets
-library(help="datasets")
